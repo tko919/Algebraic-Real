9b07f9fea4549f1b4a4899fb89f00064e4b77533
tko919 <laplace2758@gmail.com>
2021-09-11 20:40:38 +0900

    Add arithmetic operations of algebraic real

--- src/AlgReal.py
+++ src/AlgReal.py
@@ -1,78 +1,195 @@
 from src.Template import *
-from src.UniPoly import *
+from src.StrumSeq import *
 
-def StrumSeq(f):
-   pre=UniPoly(f.cs).squarefree().to_monic()
-   cur=pre.diff().to_monic()
-   ret=[pre]
-   while True:
-      if len(cur.cs)==0:
-         break
-      ret.append(cur)
-      nxt=-(pre%cur)
-      nxt=nxt.to_monic()
-      pre=UniPoly(cur.cs)
-      cur=UniPoly(nxt.cs)
-   return ret
-
-def variance(f,x):
-   seq=StrumSeq(f)
-   var=[]
-   for F in seq:
-      y=F.value(x)
-      if y==0:
-         continue
-      if y<0:
-         var.append(-1)
+def Resultant(f:UniPoly,g:UniPoly):
+   if not g.cs:
+      return f.id1()
+   if f.deg()<g.deg():
+      ret=Resultant(g,f)
+      if (f.deg()*g.deg())&1:
+         ret=-ret
+      return ret
+   k=0
+   if isinstance(g.lc(),UniPoly):
+      k=f.deg()-g.deg()+1
+      f=f.scale(g.lc()**k)
+   r=f%g
+   if not r.cs:
+      if g.deg()==0:
+         return g.cs[-1]**f.deg()
       else:
-         var.append(1)
-   ret=0
-   for i in range(len(var)-1):
-      if var[i]*var[i+1]<0:
-         ret+=1
-   return ret
-
-def CountRealroots(f,a,b):
-   return variance(f,a)-variance(f,b)
-
-def approximate(f,a,b,err):
-   assert(CountRealroots(f,a,b)==1)
-   while b-a>=err:
-      mid=(a+b)/2
-      if CountRealroots(f,a,mid)==1:
-         b=mid
+         return f.id1()
+   else:
+      ret=Resultant(g,r)
+      j=f.deg()-r.deg()-g.deg()*k
+      if j>=0:
+         ret*=g.lc()**j
       else:
-         a=mid
-   return float(a)
+         ret/=g.lc()**j
+      if (f.deg()*g.deg())&1:
+         ret=-ret
+      return ret
 
 class AlgReal:
-   def __init__(self,f,a,b) -> None:
+   def __init__(self,f=UniPoly([-1,1]),a=Fraction(1,2),b=Fraction(3,2)) -> None:
       self.f=f
       self.a=a
       self.b=b
-      pass
+      self.x=Inf
+      if self.f.value(0)==0:
+         self.x=0
+         self.f=[]
+         return
+      while self.f.cs and self.f.cs[0]==0:
+         self.f.cs.pop(0)
+      while True:
+         if 0<self.a or self.b<0:
+            break
+         mid=(self.a+self.b)
+         if self.f.value(self.a)*self.f.value(mid)<0:
+            self.b=mid
+         else:
+            self.a=mid
+      if self.f.value(self.b)==0:
+         self.x=self.b
+         self.f=[]
+      
+   def __copy__(self):
+      return AlgReal(self.f,self.a,self.b)
+   
    def __eq__(self,other):
       if self.a>other.b or self.b<other.a:
          return False
+      if not self.f and not other.f:
+         return self.x==other.x
+      elif not self.f:
+         return other.f.value(self.x)==0
+      elif not other.f:
+         return self.f.value(other.x)==0
       return CountRealroots(UniPoly.gcd(self.f,other.f)
-         ,max(self.a,other.a),min(self.b,other.b))==1
+      ,max(self.a,other.a),min(self.b,other.b))==1
 
+   def __neg__(self):
+      ret=copy.copy(self)
+      if not ret.f:
+         ret.x=-ret.x
+      else:
+         for i in range(len(ret.f.cs)):
+            if i&1:
+               ret.f.cs[i]=-ret.f.cs[i]
+         ret.a,ret.b=-ret.b,-ret.a
+      return ret
 
-def EnumRoots(f,a=-Inf,b=Inf):
-   mx=0
-   for c in f.cs:
-      mx=max(mx,abs(c))
-   Bound=mx/abs(f.cs[-1])+1
-   a=max(min(a,Bound),-Bound)
-   b=max(min(b,Bound),-Bound)
+   def __add__(self,other):
+      ret=copy.copy(self)
+      if not isinstance(other,AlgReal):
+         ret.f=UniPoly.comp(ret.f,UniPoly([-other,1]))
+         ret.a+=other
+         ret.b+=other
+      elif not self.f and not other.f:
+         ret.x+=other.x
+      elif not self.f:
+         ret=other+ret.x
+      elif not other.f:
+         ret+=other.x
+      else:
+         f=UniPoly.comp(self.f,UniPoly([[0,1],[-1]]))
+         g=UniPoly.comp(other.f,UniPoly([[],[1]]))
+         ret.f=Resultant(f,g).squarefree()
+         n=ret.f.deg()
+         D=np.sqrt(float(abs(Resultant(ret.f,ret.f.diff())/ret.f.lc())))
+         m=D/(abs(ret.f.lc())**(n-1)*(BoundOfRoots(ret.f)*2)**(n*(n-1)//2-1))
+         a,b,c,d=self.a,self.b,other.a,other.b
+         while True:
+            ret.a,ret.b=a+c,b+d
+            if ret.b-ret.a<m:
+               break
+            mid=(a+b)/2
+            if self.f.value(a)*self.f.value(mid)<=0:
+               b=mid
+            else:
+               a=mid
+            mid=(c+d)/2
+            if other.f.value(c)*other.f.value(mid)<=0:
+               d=mid
+            else:
+               c=mid    
+      return ret
+
+   def __sub__(self,other):
+      return self+(-other)
+
+   def __mul__(self,other):
+      ret=copy.copy(self)
+      if not isinstance(other,AlgReal):
+         q=1
+         for i in range(len(ret.f.cs)):
+            ret.f.cs[i]/=q
+            q*=other
+         ret.a*=other
+         ret.b*=other
+         if other<0:
+            ret.a,ret.b=ret.b,ret.a
+      if not self.f and not other.f:
+         ret.x*=other.x
+      elif not self.f:
+         ret=other*ret.x
+      elif not other.f:
+         ret*=other.x
+      else:
+         base=[]
+         n=self.f.deg()
+         for i in range(n+1):
+            add=UniPoly([self.f.cs[n-i]])
+            base.append(add.shift(n-i))
+         f=UniPoly(base)
+         g=UniPoly.comp(other.f,UniPoly([[],[1]]))
+         ret.f=Resultant(f,g).squarefree()
+         n=ret.f.deg()
+         D=np.sqrt(float(abs(Resultant(ret.f,ret.f.diff())/ret.f.lc())))
+         m=D/(abs(ret.f.lc())**(n-1)*(BoundOfRoots(ret.f)*2)**(n*(n-1)//2-1))
+         a,b,c,d=self.a,self.b,other.a,other.b
+         while True:
+            ret.a,ret.b=min(a*c,a*d,b*c,b*d),max(a*c,a*d,b*c,b*d)
+            if ret.b-ret.a<m:
+               break
+            mid=(a+b)/2
+            if self.f.value(a)*self.f.value(mid)<=0:
+               b=mid
+            else:
+               a=mid
+            mid=(c+d)/2
+            if other.f.value(c)*other.f.value(mid)<=0:
+               d=mid
+            else:
+               c=mid    
+      return ret
    
-   if CountRealroots(f,a,b)==0:
+   def inv(self):
+      ret=copy.copy(self)
+      ret.f.cs.reverse()
+      ret.a,ret.b=1/ret.b,1/ret.a
+      return ret
+
+   def __truediv__(self,other):
+      return self*other.inv()
+
+
+def EnumRoots(f,a=-Inf,b=Inf,seq=[]):
+   if a==-Inf:
+      Bound=BoundOfRoots(f)
+      a=max(min(a,Bound),-Bound)
+      b=max(min(b,Bound),-Bound)
+   if not seq:
+      seq=StrumSeq(f)
+   
+   if CountRealroots(f,a,b,seq)==0:
       return []
-   elif CountRealroots(f,a,b)==1:
+   elif CountRealroots(f,a,b,seq)==1:
       return [AlgReal(f,a,b)]
    else:
       mid=(a+b)/2
-      ret=EnumRoots(f,a,mid)
-      for x in EnumRoots(f,mid,b):
+      ret=EnumRoots(f,a,mid,seq)
+      for x in EnumRoots(f,mid,b,seq):
          ret.append(x)
       return ret
--- src/UniPoly.py
+++ src/UniPoly.py
@@ -1,103 +1,186 @@
+from numpy.lib.arraysetops import isin
 from src.Template import *
 
 class UniPoly:
-   def __init__(self,cs) -> None:
+   def __init__(self,cs,Value=Fraction) -> None:
       self.cs=[]
+      self.v=Value
+      if not isinstance(cs,list):
+         cs=[cs]
       for c in cs:
-         self.cs.append(Fraction(c))
-      while len(self.cs)>0 and self.cs[-1]==0:
+         if isinstance(c,list):
+            c=UniPoly(c)
+         if isinstance(c,UniPoly):
+            self.cs.append(c)
+            self.v=UniPoly
+         else:
+            self.cs.append(Value(c))
+      while self.cs and self.lc()==self.id1():
          self.cs.pop(-1)
-      pass
-   def id():
-      return UniPoly([0,1])
+       
+   def __copy__(self):
+      return UniPoly(self.cs,self.v)
+
+   def __eq__(self,other):
+      if not isinstance(other,UniPoly) or self.deg()!=other.deg():
+         return False
+      for i in range(self.deg()+1):
+         if self.cs[i]!=other.cs[i]:
+            return False
+      return True
+
+   def lc(self):
+      return self.cs[-1]
+
+   def id1(self):
+      if self.v==UniPoly:
+         return UniPoly([])
+      else:
+         return self.v(0)
+         
+   def id2(self):
+      if self.v==UniPoly:
+         return UniPoly([self.lc().id2()])
+      else:
+         return self.v(1)
+         
    def deg(self):
       return len(self.cs)-1
+
    def scale(self,a):
-      ret=UniPoly(self.cs)
+      ret=copy.copy(self)
       for i in range(len(self.cs)):
          ret.cs[i]*=a
       return ret
+
    def to_monic(self):
-      if len(self.cs)==0:
+      if not self.cs:
          return self
-      lc = abs(self.cs[-1])
-      return UniPoly.scale(self,1/lc)
+      return UniPoly.scale(self,1/self.lc())
+
    def shift(self,a):
-      ret=UniPoly(self.cs)
-      ret.cs[0:0]=[0]*a
+      ret=copy.copy(self)
+      ret.cs[0:0]=[ret.id1()]*a
       return ret
+
    def value(self,x):
-      ret=0
+      ret=self.id1()
       for c in reversed(self.cs):
          ret*=x
          ret+=c
       return ret
-   def debug(self):
-      print(self.cs)
+
    def __neg__(self):
-      ret=UniPoly(self.cs)
+      ret=copy.copy(self)
       for i in range(len(self.cs)):
          ret.cs[i]=-ret.cs[i]
       return ret
 
    def __add__(self,other):
+      if not isinstance(other,UniPoly):
+         ret=copy.copy(self)
+         if not ret.cs:
+            ret.cs.append(self.v(0))
+         ret.cs[0]+=other
+         return ret
       L=max(len(self.cs),len(other.cs))
-      ret=[0]*L
+      ret=[self.id1()]*L
       for i in range(L):
          if i<len(self.cs):
             ret[i]+=self.cs[i]
          if i<len(other.cs):
             ret[i]+=other.cs[i]
       return UniPoly(ret)
+
    def __sub__(self,other):
       L=max(len(self.cs),len(other.cs))
-      ret=[0]*L
+      ret=[self.id1()]*L
       for i in range(L):
          if i<len(self.cs):
             ret[i]+=self.cs[i]
          if i<len(other.cs):
             ret[i]-=other.cs[i]
       return UniPoly(ret)
+
    def __mul__(self,other):
+      if not isinstance(other,UniPoly):
+         return self.scale(other)
+      if not self.cs or not other.cs:
+         return UniPoly([])
       L=len(self.cs)+len(other.cs)-1
-      ret=[0]*L
+      ret=[other.id1()]*L
       for i in range(len(self.cs)):
          for j in range(len(other.cs)):
-            ret[i+j]+=self.cs[i]*other.cs[j]
+            if isinstance(self.cs[i],UniPoly):
+               ret[i+j]+=self.cs[i]*other.cs[j]
+            else:
+               ret[i+j]+=other.cs[j]*self.cs[i]
       return UniPoly(ret)
+
    def __truediv__(self,other):
-      ret=[0]*(len(self.cs)-len(other.cs)+1)
-      f,g=UniPoly(self.cs),UniPoly(other.cs)
+      if not self.cs or not other.cs:
+         return UniPoly([])
+      ret=[self.id1()]*(len(self.cs)-len(other.cs)+1)
+      f,g=copy.copy(self),copy.copy(other)
       while len(f.cs)>=len(g.cs):
-         p,q=f.cs[-1],g.cs[-1]
+         p,q=f.lc(),g.lc()
          shift=len(f.cs)-len(g.cs)
          ret[shift]=p/q
-         sub=UniPoly(g.cs).scale(p/q).shift(shift)
+         sub=g.scale(p/q).shift(shift)
          f-=sub
       return UniPoly(ret)
+
    def __mod__(self,other):
       h=self/other
       ret=self-h*other
       return ret
+
+   def __pow__(self,t):
+      ret=UniPoly([self.id2()])
+      b=copy.copy(self)
+      while t>0:
+         if t&1:
+            ret*=b
+         b*=b
+         t>>=1
+      return ret
+
    def comp(f,g):
-      ret=UniPoly([0])
+      ret=UniPoly([f.id1()])
       for c in reversed(f.cs):
          ret*=g
-         ret=UniPoly.scale(ret,c)
+         ret+=c
       return ret
+
    def gcd(f,g):
-      if len(g.cs)==0:
+      if not g.cs:
          return f
       else:
          h=f%g
          return UniPoly.gcd(g,h.to_monic())
+
    def diff(self):
-      ret=UniPoly(self.cs)
-      if len(ret.cs)>0:
+      ret=copy.copy(self)
+      if ret.cs:
          ret.cs.pop(0)
       for i in range(len(ret.cs)):
          ret.cs[i]*=(i+1)
       return ret
+
    def squarefree(self):
-      g=UniPoly.diff(self)
+      g=self.diff()
       return self/UniPoly.gcd(self,g)
+
+def debug(f,End='\n'):
+   if isinstance(f,UniPoly):
+      print('[',end='')
+      for i in range(len(f.cs)):
+         debug(f.cs[i],(',' if i!=len(f.cs)-1 else ''))
+      print(end=']'+End)
+   elif isinstance(f,list):
+      print('[',end='')
+      for i in range(len(f)):
+         debug(f[i],(',' if i!=len(f)-1 else ''))
+      print(end=']'+End)
+   else:
+      print(f,end=End)
